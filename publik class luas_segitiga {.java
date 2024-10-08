e4d8c7e98168323aae5cd8e0277ac65782f454a6
nisa-knf15 <nisafitriani39@gmail.com>
2024-10-08 13:41:05 +0700

    Add files via upload

---
+++ publik class luas_segitiga {.java
@@ -0,0 +1,16 @@
+publik class luas_segitiga {
+    run | Debug 
+    publik static void main (String[] args) {
+        //deklarasi variabel
+        double alas, tinggi, luas;
+
+        //inisialisasi variabel
+        alas=5;
+        tinggi=10;
+        luas=0,5*alas*tinggi;
+
+        //cetak hasil
+        system.out.println("luas segitiga adalah'+luas");
+        
+    }
+}
